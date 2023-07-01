4e20d8073c8eb09e0ef1d011dd81557ca74bc369
Austin Daniels <88160796+notaustindaniels@users.noreply.github.com>
2023-07-01 16:03:36 +0000

    Incorporates Bootstrap

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,40 +1,37 @@
 import React, { useState } from "react";
 
 const Home = () => {
-	const [inputValue, setInputValue] = useState("");
-	const [todos, setTodos] = useState([]);
-	return (
-		<div className="container">
-			<h1>todos</h1>
-			<ul>
-				<li>
-					<input
-					 type="text"
-					 onChange = {(e) => setInputValue(e.target.value)}
-					 value = {inputValue}
-					 onKeyDown = {(e) => {
-						if(e.key === 'Enter') {
-							setTodos(todos.concat(inputValue));
-							setInputValue("");
-						}
-					 }}	
-					 placeholder="What do you need to do?"></input>
-				</li>
-				{todos.map((item, index) => (
-					<li>
-						{item}{" "}
-						<i
-							class="fas fa-trash-alt"
-							onClick={() => 
-							setTodos(todos.filter((t, currentIndex) =>
-								index != currentIndex))}>
-						</i>
-					</li>
-				))}
-			</ul>
-			<div>{todos.length} Tasks</div>
-		</div>
-	)
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    return (
+        <div id="container">
+            <h1 className="todo-header">To do List</h1>
+            <input
+                id="addToDo"
+                type="text"
+                placeholder="Add to do here"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                        setTodos(todos.concat(inputValue));
+                        setInputValue("");
+                    }
+                }}
+            />
+            <ul>
+                {todos.map((item, index) => (
+                    <li key={index}>
+                        <span>
+                            <i className="fa fa-trash" onClick={() => setTodos(todos.filter((t, currentIndex) => index !== currentIndex))}></i>
+                        </span>
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default Home;
